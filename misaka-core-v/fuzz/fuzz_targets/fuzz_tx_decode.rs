ba6131246f1bb70946b9e59ee22623bf719994e8
Misaka <retadashi.for@gmail.com>
2026-03-19 18:03:17 +0900

    Delete misaka-core-v directory

--- misaka-core-v/fuzz/fuzz_targets/fuzz_tx_decode.rs
+++
@@ -1,7 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    // Must never panic, regardless of input
-    let _ = misaka_pqc::tx_codec::decode_transaction(data);
-});
