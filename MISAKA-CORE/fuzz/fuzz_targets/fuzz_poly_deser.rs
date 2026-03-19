9e2822fdec409775f0c22a394db56625ce7e2c89
Misaka <retadashi.for@gmail.com>
2026-03-19 18:41:09 +0900

    Delete MISAKA-CORE directory

--- MISAKA-CORE/fuzz/fuzz_targets/fuzz_poly_deser.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-use misaka_pqc::pq_ring::Poly;
-
-fuzz_target!(|data: &[u8]| {
-    // Must never panic, regardless of input
-    let _ = Poly::from_bytes(data);
-});
